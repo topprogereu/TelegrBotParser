c6edbdfe7d485f0740fb014ca6720da4d722e438
Pasha <topprogereu@gmail.com>
2019-06-04 19:38:11 +0300

    Start

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Main extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+
+    }
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return null;
+    }
+
+    public String getBotToken() {
+        return null;
+    }
+}
