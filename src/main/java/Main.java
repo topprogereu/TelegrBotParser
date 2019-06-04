9f9ebaaf15a35c91037b2635a28f5954878e4e52
Pasha <topprogereu@gmail.com>
2019-06-04 23:32:23 +0300

    Start party hard

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,24 @@
 
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
-public class Main extends TelegramLongPollingBot {
+public class Main  {
     public static void main(String[] args) {
 
-    }
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
 
-    public void onUpdateReceived(Update update) {
+        try {
+            botsApi.registerBot(new MyAmazingBot());
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
-    }
-
-    public String getBotUsername() {
-        return null;
-    }
-
-    public String getBotToken() {
-        return null;
     }
 }
